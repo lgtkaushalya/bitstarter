ddf975898a165f5b6ab4e615c3a6e79151785b7f
thilanka <lgtkaushlaya@gmail.com>
2013-07-14 11:59:10 +0530

    Added resulting index elements to check.json

--- grader.js
+++ grader.js
@@ -30,7 +30,7 @@ var checkHtmlFile = function (htmlfile, checksfile) {
     var out = {};
     
     for (var ii in checks) {
-        var present = $(checks[ii].length > 0);
+        var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;    
     }
     return out;
